09369f67522867468109dac2c3c27a178d2a202f
daobee <chjr93820@gmail.com>
2021-08-02 08:31:03 +0800

    deploy funcition

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, Link } from "react-router-dom";
+import IdolList from './IdolList';
+import ModList from './ModList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ul id="navigation">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+          <Link to="/about">About</Link>
+          </li>
+          <li>
+          <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Switch>
+          <Route exact path="/">
+            <IdolList />
+          </Route>
+          <Route path="/about">
+            <ModList />
+          </Route>
+        </Switch>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/ModList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const divStyle = {
+    color:'white'
+};
+
+class ModList extends React.Component {
+    
+    render() {
+        return (
+            <div style={divStyle}>
+                <h2>ModList Page</h2>
+                <main>
+                    <p>This section contains information about...</p>
+                </main>
+            </div>
+        )
+    }
+}
+
+
+
+export default ModList;
