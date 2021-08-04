f34c97fd8ff1eb1c9d6a0060c1c61a71380edaed
daobee <chjr93820@gmail.com>
2021-08-04 11:48:39 +0800

    added dropzone

---
+++ src/Pages/IdolEditor.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import Dexie from 'dexie';
+import Dropzone from 'react-dropzone'
+import { withRouter } from 'react-router-dom';
+import { AwesomeButton as Button } from "react-awesome-button";
+import { toast } from 'react-toastify';
+
+class IdolEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        // initDbConnection
+        this.db = new Dexie('Idols');
+        this.db.version(1).stores({ friends: "++id,name,age" });
+        this.state = {file: null};
+        this.handleFile = this.handleFile.bind(this);
+    }
+
+    addIdol() {
+        const db = this.db;
+        db.transaction('rw', db.friends, async() => {
+            // Make sure we have something in DB:
+            if ((await db.friends.where({name: 'Josephine'}).count()) === 0) {
+                const id = await db.friends.add({name: "Josephine", age: 21});
+                toast (`Addded friend with id ${id}`);
+            } else {
+                const id = await db.friends.add({name: "Josephine", age: 21 + Math.random()});
+                toast (`Addded friend with id ${id}`);
+            }
+            // Query:
+            const youngFriends = await db.friends.where("age").below(25).toArray();
+            // Show result:
+            toast ("My young friends: " + JSON.stringify(youngFriends));
+        
+        }).catch(e => {
+            toast(e.stack || e);
+        });
+        toast('add idol!', {
+            position: toast.POSITION.BOTTOM_RIGHT
+        })
+    }
+
+    handleFile(acceptedFiles) {
+        const imageType = /^image\//;
+        if (Array.isArray(acceptedFiles)) {
+            if (imageType.test(acceptedFiles[0].type)) {
+                this.setState({
+                    file: acceptedFiles[0]
+                }, () => console.log(this.state.file))
+            } else toast('不支持的格式！');
+        }
+    }
+
+    render() {
+        const { file } = this.state;
+        return (
+            <div style={{ color: "black"}}>
+                <Button type="primary" onPress={ () => this.props.history.goBack() } >Back</Button>
+                <Button type="primary" onPress={ () => this.addIdol() } >Button</Button>
+                <Dropzone onDrop={ this.handleFile }>
+                    {({getRootProps, getInputProps, isDragActive}) => (
+                        <section>
+                            <div {...getRootProps()}>
+                                <h2>IdolEditor Page</h2>
+                                <p>IdolEditor about...</p>
+                                { file ? <img src={ window.URL.createObjectURL(file) } alt="idol" /> : null}
+                                <input {...getInputProps()} />
+                                {
+                                    isDragActive ?
+                                    <p>Drop the files here ...</p> :
+                                    <p>Drag 'n' drop some files here, or click to select files</p>
+                                }
+                            </div>
+                        </section>
+                    )}
+                </Dropzone>
+            </div>
+        )
+    }
+}
+
+
+
+export default withRouter(IdolEditor);
--- src/Pages/Routes.js
+++ src/Pages/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import IdolList from './IdolList';
 import ModList from './ModList';
+import IdolEditor from './IdolEditor';
 
 class Routes extends React.Component {
   render () {
@@ -10,7 +11,8 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path="/"><IdolList /></Route>
           <Route exact path="/idolList"><IdolList /></Route>
-          <Route path="/modList"><ModList /></Route>
+          <Route exact path="/modList"><ModList /></Route>
+          <Route exact path="/idolEditor"><IdolEditor /></Route>
         </Switch>
       </div>
     );
